1974c2596d26b68e7cbd44a6cc3e3e3489859a0a
syahrulpratama665-joy <syahrulpratama665@gmail.com>
2026-07-26 11:07:46 +0700

    Use web auth guard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->authGuard('web')
             ->colors([
                 'primary' => Color::Amber,
             ])
