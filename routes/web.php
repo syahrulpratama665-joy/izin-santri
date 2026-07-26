72987ba52c1e148b8a59f8b7572935109d028a9c
syahrulpratama665-joy <syahrulpratama665@gmail.com>
2026-07-26 10:44:15 +0700

    Add auth test route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-login', function () {
+    return [
+        'user' => Auth::user(),
+        'check' => Auth::check(),
+    ];
 });
